9d101b4d0af2dfa02ffeaa46e4d9255ddc0566dd
cuellar2020 <67763251+cuellar2020@users.noreply.github.com>
2022-05-18 15:05:28 -0400

    Add project files.

---
+++ lab09/lab09/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace lab09
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            item01.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new Views.TapDemo());
+            };
+
+            item02.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new Views.PinchDemo());
+            };
+
+            item03.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new Views.PanDemo());
+            };
+
+            item04.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new Views.SwipeDemo());
+            };
+        }
+    }
+}
---
+++ lab09/lab09/Views/TapDemo.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lab09.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TapDemo : ContentPage
+    {
+        int tapCount;
+        readonly Label label;
+        public TapDemo()
+        {
+            
+            InitializeComponent();
+            var image = new Image
+            {
+                Source = "abraham_lincoln_color.jpg",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                WidthRequest = 300,
+                HeightRequest = 500
+            };
+
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.NumberOfTapsRequired = 2;
+            tapGestureRecognizer.Tapped += OnTapGestureRecognizerTapped;
+            image.GestureRecognizers.Add(tapGestureRecognizer);
+
+
+            label = new Label
+            {
+                Text = "tap the photo!",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            Content = new StackLayout
+            {
+                Padding = new Thickness(20),
+                Children = { image, label },
+            };
+        }
+
+        void OnTapGestureRecognizerTapped(object sender, EventArgs e)
+        {
+            tapCount++;
+            label.Text = String.Format("{0} tap{1} so far!", tapCount, tapCount == 1 ? "" : "s");
+
+            var imageSender = (Image)sender;
+
+            if (tapCount % 2 == 0)
+            {
+                imageSender.Source = "p_color.jpg";
+            }
+            else
+            {
+                imageSender.Source = "p_sin.jpg";
+            }
+        }
+    }
+}
